8d8589f2e71435c47738bf1e1698042c0d86e02e
zyb1x <dluis98@hotmail.com>
2025-10-07 20:04:34 -0600

    .

--- app/src/main/java/com/example/canchibol/MainActivity.kt
+++ app/src/main/java/com/example/canchibol/MainActivity.kt
@@ -18,30 +18,9 @@ class MainActivity : ComponentActivity() {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContent {
-            CanchiBolTheme {
-                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-                    Greeting(
-                        name = "Android",
-                        modifier = Modifier.padding(innerPadding)
-                    )
-                }
-            }
+
         }
     }
 }
 
-@Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
-}
 
-@Preview(showBackground = true)
-@Composable
-fun GreetingPreview() {
-    CanchiBolTheme {
-        Greeting("Android")
-    }
-}
